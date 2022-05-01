4fd83f8a2e129316d4d3963053fe713e073295d6
yutteee <yusaku_nakamura_aa@icloud.com>
2022-05-01 14:29:53 +0900

    デザインをちょっと変更

---
+++ static/pray.js
@@ -0,0 +1,6 @@
+var music = new Audio('../templates/sousiki1.mp3');
+
+function play() {
+    // music.play();
+    console.log('aaa')
+}
---
+++ templates/pray.js
@@ -0,0 +1,9 @@
+var music = new Audio('sousiki1.mp3');
+
+// document.getElementsByClassName('button').addEventListener('click', () => {
+//     console.log('aaa');
+// })
+
+function play() {
+    music.play();
+}
